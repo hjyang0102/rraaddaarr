0bdf232dea0ae20e96109b724c0795114687d948
dumbdumbdumb <31528225+dumbdumbdumb@users.noreply.github.com>
2025-02-03 11:28:00 +0900

    Update recModule.py

--- recModule/recModule.py
+++ recModule/recModule.py
@@ -25,20 +25,13 @@ class recommendation(nn.Module):
 
         self.num_entities = kg['num_entities']  #24636
 
-
-
         #self.ent_embs_in_kg = self.KG_encoder()  #[24636,128]
 
         self.nnlayer_for_plot = nn.Linear(1024, 128, bias=False)
 
-        #self.nnlayer_for_context = nn.Linear(1024, 128, bias=False) #FlanT5_emb_size, R-GCN_emb_size
-        self.fc1 = nn.Linear(1024, 768, bias=False)
-        self.fc2 = nn.Linear(768, 512, bias=False)
-        self.fc3 = nn.Linear(512, 128, bias=False)
-        self.relu = nn.ReLU()
+        self.nnlayer_for_context = nn.Linear(1024, 128, bias=False) #FlanT5_emb_size, R-GCN_emb_size
 
         self.item_plot_embeddings = torch.tensor(np.load('item_plot_embeddings.npy')).to(device).detach() #[24636,1024]
-
 
         self.item_list = []
         with open('item_ids.json','r',encoding='utf-8') as f:
@@ -49,7 +42,6 @@ class recommendation(nn.Module):
     def generate_ent_item_plot_embs(self):
         ent_embs_in_kg = self.KG_encoder() #[24631,128]
 
-
         item_plot_embeddings = self.nnlayer_for_plot(self.item_plot_embeddings) #[24631,1024] > [24631,128]
 
         ent_item_plot_embs = torch.cat((ent_embs_in_kg,item_plot_embeddings), dim=1)
@@ -58,7 +50,6 @@ class recommendation(nn.Module):
 
 
     def generate_rec_label(self,rec_label):
-
         batch_size = rec_label.size()[0]
         labels = torch.zeros(batch_size,self.num_entities)
 
@@ -68,22 +59,17 @@ class recommendation(nn.Module):
         return labels
 
     def generate_rec_label_multiple(self,rec_label):
-
         list_size = len(rec_label)
         batch_size = rec_label[0].size()[0]
         labels = torch.zeros(batch_size,self.num_entities)
-
 
         for i in range(batch_size):
             for batch_count, tensor in enumerate(rec_label):
                 labels[i][tensor[i].item()]=1
 
-
-
         return labels
 
     def generate_preferred_attr_label(self,preferred_attr):
-
         batch_size = preferred_attr.size()[0]
         labels = torch.zeros(batch_size,self.num_entities)
 
@@ -99,20 +85,13 @@ class recommendation(nn.Module):
         return torch.mm(weights, value)
 
     def get_nn_encoded_context_embs(self, encoded_context_embs):
-        #return self.nnlayer_for_context(encoded_context_embs)
-        x = self.fc1(encoded_context_embs)
-        x = self.relu(x)
-
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
-        return x
+        return self.nnlayer_for_context(encoded_context_embs)
+    
     def generate_user_emb_for_preferred_attr(self, ent_list, context_ids):
         ent_embs = self.KG_encoder() # device:0
 
         bs_size = ent_list[0].size()[0]
         list_size = len(ent_list)
-
 
         uset_emb_list = []
         for i in range(bs_size):
@@ -141,7 +120,6 @@ class recommendation(nn.Module):
 
         user_embs = torch.cat(uset_emb_list, dim=0)
 
-
         output = self.smallerLM.encoder(context_ids)
         encoded_context_embs = output.last_hidden_state.mean(dim=1)
 
@@ -155,10 +133,7 @@ class recommendation(nn.Module):
         user_embs_for_kg, encoded_context_embs = self.generate_user_emb_for_preferred_attr(ent_list, context_ids)
         user_embs_for_lm = self.get_nn_encoded_context_embs(encoded_context_embs)
 
-
         return torch.cat((user_embs_for_kg,user_embs_for_lm), dim=1)
-
-
 
     def cal_logits_for_preferred_attr(self, user_embs):
         ent_embs = self.KG_encoder()  #[24631,128]
@@ -173,16 +148,13 @@ class recommendation(nn.Module):
 
         return logits
 
-
     def exclude_attrs_only_items_for_rec_training(self, labels):
         mask = torch.ones_like(labels, dtype=torch.bool)
         mask[:, self.item_list] = False
 
-
         masked_labels = labels.masked_fill(mask, float('-inf')) #Set to -inf to exclude them
 
         return masked_labels
-
 
     def training_preferred_attrs(self,ent_list,preferred_attr, context_ids):
         preferred_attr_labels = self.generate_preferred_attr_label(preferred_attr)
@@ -198,38 +170,29 @@ class recommendation(nn.Module):
         return preferred_attr_loss
 
     def training_recommendation(self, ent_list, rec_label, context_ids):
-
         recommendation_labels = self.generate_rec_label(rec_label)
         user_embs = self.generate_user_emb_for_recommendation(ent_list, context_ids)
 
         rec_logits = self.cal_logits_for_recommendation(user_embs)
 
-
         recommendation_labels = recommendation_labels.to(device).detach()
-
 
         recommendation_loss = F.cross_entropy(rec_logits, recommendation_labels, reduction='mean')
 
         return recommendation_loss
 
     def training_recommendation_multiple_rec(self, ent_list, rec_label, context_ids, rec_items):
-
-
         user_embs = self.generate_user_emb_for_recommendation(ent_list, context_ids)
         recommendation_labels = self.generate_rec_label_multiple(rec_items)
 
         rec_logits = self.cal_logits_for_recommendation(user_embs)
 
-
         recommendation_labels = recommendation_labels.to(device).detach()
-
 
         bce_criterion = torch.nn.MultiLabelSoftMarginLoss(reduction='mean')
         recommendation_loss = bce_criterion(rec_logits, recommendation_labels)
 
         return recommendation_loss
-
-
 
     def eval_recommendation(self, ent_list, rec_label, context_ids, rec_items):
 
@@ -242,7 +205,6 @@ class recommendation(nn.Module):
         masked_logits = rec_logits.masked_fill(mask, float('-inf'))
 
         ranks = torch.topk(masked_logits, k=50, dim=-1).indices
-
 
         return ranks, rec_label
 
